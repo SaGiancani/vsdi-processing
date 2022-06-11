c22be936375b92c6b1702c018426114651eb73ba
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2022-06-11 15:28:44 +0000

    indeces adjusted -list not np.array-

--- middle_process.py
+++ middle_process.py
@@ -212,7 +212,7 @@ class Session:
                 self.chunk_distribution_visualization(b, d, c, c_, strategy)
                 # Coming back to the previous indexing system: not indexing intracondition, but indexing in tc matrix with all the conditions
                 tmp[indeces[t].tolist()] = 1
-            
+            tmp = tmp.tolist()
         elif strategy in ['roi', 'roi_signals', 'ROI']:
             tmp = roi_strategy(self.time_course_signals[self.counter_blank:, :], self.header['tolerance'], self.header['zero_frames'])
 
@@ -230,7 +230,7 @@ class Session:
         if save_switch:
             np.save('time_courses.npy', self.time_course_signals)
         
-        self.log.info(str(sum(self.auto_selected)) + '/' + str(len(self.session_blks)) +' trials have been selected!')
+        self.log.info(str(int(sum(self.auto_selected))) + '/' + str(len(self.session_blks)) +' trials have been selected!')
         session_blks = np.array(self.session_blks)
         self.trials_name = session_blks[self.auto_selected]
         self.log.info(np.array(self.conditions)[self.auto_selected])
