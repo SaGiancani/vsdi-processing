90b2368cf124e8162c2e3764994c497dc0a83222
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2022-06-21 09:51:12 +0000

    Class Trial for log files' information implemented

--- ana_logs.py
+++ ana_logs.py
@@ -1,13 +1,49 @@
-import datetime, utils
+import datetime, imageio, os, utils
 import middle_process as mp
 import numpy as np
 import pandas as pd
 
-class Trial:
-    def __init__(self, report_series_trial):
-        self.blk = None
-        self.name = report_series_trial['BLK Names']
 
+class Trial:
+    def __init__(self, report_series_trial, heart, piezo, session_path, blank_cond, log = None, stimulus_fr = None, zero_fr = None, time_res = 10, blk_file = None):
+        self.blk = blk_file
+        self.name = report_series_trial['BLK Names']
+        self.condition = int(report_series_trial['IDcondition'])
+        self.fix_correct = report_series_trial['Preceding Event IT'] == 'FixCorrect'
+        self.correct_behav = report_series_trial['behav Correct'] == 1
+        self.id_trial = int(report_series_trial['Total Trial Number']) - 1
+        pngfile_path = utils.find_thing('PNGfiles', os.path.join(session_path, 'sources'))
+
+        if self.condition != blank_cond:
+            try:
+                grey_frames_start, grey_frames_end, _ = get_grey_frames(pngfile_path, self.condition)
+                #n_pngs = pngs_shape[0]
+            except:
+                if log is None:
+                    print('Issue with PNGfiles: standard values 5 grey frames pre stimulus and 5 post will be used')
+                else:
+                    log.info('Issue with PNGfiles: standard values 5 grey frames pre stimulus and 5 post will be used')
+                grey_frames_start = 5
+                grey_frames_end = 5
+
+            if stimulus_fr is None:
+                # Total registration time (End registration (PNG flow + Post Stimulus time) - Start registration) - Starting Grey frames*temporal resolution - Ending Grey frames*temporal resolution + 25 ms latency 
+                stimulus_fr = round((separator_converter(report_series_trial['Onset Time_ End Stim']) - separator_converter(report_series_trial['Onset Time_ Stim']) - grey_frames_end*time_res - grey_frames_start*time_res + 25)/time_res)         
+            
+            self.FOI = stimulus_fr
+
+            if zero_fr is None:
+                #PreStimulus Time + nGreyFrames*10ms + 25ms Response Latency
+                zero_fr = round(((report_series_trial['Onset Time_ Stim'] - report_series_trial['Onset Time_ Pre Stim']) + 25 + grey_frames_start*time_res)/time_res) 
+            
+            self.zero_frames = zero_fr
+
+        else:
+            self.zero_frames = self.blk.header['nframesperstim']
+            self.FOI = self.zero_frames
+
+        self.heart_signal = heart
+        self.piezo_signal = piezo
 
 def add_blknames2basereport(BaseReport, all_blks):
     '''
@@ -42,7 +78,8 @@ def add_blknames2basereport(BaseReport, all_blks):
                 print('A Missing row is added')            
             elif (len(all_blks)>len(cds)):
                 all_blks.pop(tris[0])
-                print(f'File {tris[2]} is deleted')            
+                print(f'File {tris[2]} is deleted')
+                print('Take care to time course and dF/F0 matrix indeces and indexing system.')            
             print(tris)
         # Consider the BLK names, in case of FixCorrect preceding event IT
         BaseReport.loc[BaseReport['Preceding Event IT'] == 'FixCorrect', 'BLK Names'] = all_blks
@@ -53,12 +90,11 @@ def get_basereport(session_path, name_report = 'BaseReport.csv', header_dimensio
     '''
     Load the BaseReport
     '''
-    BaseReport_path = utils.find_file(name_report, session_path)
+    BaseReport_path = utils.find_thing(name_report, session_path, what = 'file')
     BaseReport = pd.read_csv(BaseReport_path[0], sep=';', header=header_dimension)
     #Adding BLK Names columns to the dataframe
     BaseReport, tris = add_blknames2basereport(BaseReport, mp.get_all_blks(session_path, sort = True))
     return BaseReport, tris
-
 
 def get_basereport_header(BaseReport_path, header_dimension = 19):    
     '''
@@ -71,7 +107,7 @@ def get_basereport_header(BaseReport_path, header_dimension = 19):
         tmp = tmp.split('\n')[0].split(';')
         if '*' not in tmp[0]:
             try:
-                dict_[tmp[0]] = toogle_from_object(tmp[1].split('\n')[0])
+                dict_[tmp[0]] = separator_converter(tmp[1].split('\n')[0])
             except:
                 dict_[tmp[0]] = tmp[1].split('\n')[0]
     dict_['Export Log Files'] = bool(dict_['Export Log Files'])
@@ -80,12 +116,77 @@ def get_basereport_header(BaseReport_path, header_dimension = 19):
     return dict_
 
 
-def toogle_from_object(s):
+def get_analog_signal(analog_sign_path, BaseReport, name_report = 'SignalData.csv'):
+    SignalData = pd.read_csv(os.path.join(analog_sign_path, name_report), sep=';', header=2)
+    # Timestamp importing
+    temp = SignalData[['Timestamp','Dev1/ai5', 'Dev1/ai6' ]].applymap(separator_converter)
+    analog_timestamp_array = np.array(temp['Timestamp'])
+    # HeartBeat
+    analog_ai5_array = np.array(temp['Dev1/ai5'])
+    # Piezo
+    analog_ai6_array = np.array(temp['Dev1/ai6'])
+
+    t = BaseReport[['Onset Time_ Pre Trial', 'Onset Time_ End Trial']].applymap(separator_converter)
+    onset_pre = t['Onset Time_ Pre Trial'].tolist()
+    onset_end = t['Onset Time_ End Trial'].tolist()
+    pres, ends = [], []
+    
+    for pre, end in zip(onset_pre, onset_end):
+        # This is only for finding the start and end indeces, corresponding to the value obtained
+        pres.append(np.argmin(np.abs(analog_timestamp_array - pre)))
+        ends.append(np.argmin(np.abs(analog_timestamp_array - (end + 1000))))#   +1sec for safety
+    tracks_6, tracks_5 = [], []
+    for p, e in zip(pres, ends):
+        # Piezo
+        tracks_6.append(np.array(analog_ai6_array[p:e]))
+        # HeartBeat
+        tracks_5.append(np.array(analog_ai5_array[p:e]))    
+    return (pres, ends), tracks_6, tracks_5
+
+
+def get_grey_frames(png_files_path, cond_id):
+    start_time = datetime.datetime.now().replace(microsecond=0)
+    all_pngs =  [f.name for f in os.scandir(png_files_path) if (f.is_file()) and (f.name.startswith("Cond")) and (f.name.split('Cond') and int(f.name.split('-')[0].split('Cond')[1]) == cond_id)]
+
+    a = np.zeros((len(all_pngs), 256, 256, 3))
+    for i, png in enumerate(all_pngs):
+        im = imageio.imread(os.path.join(png_files_path, png))
+        a[i, :, :, :] = im
+
+    out = list()
+    final_out = list()
+
+    for count, i in enumerate(range(len(a)-1, 0, -1)):
+        if not np.array_equal(a[count], a[count+1]):
+                out.append(count+1)
+        if not np.array_equal(a[i], a[i-1]):
+                final_out.append(count+1)
+    print('Time for png evaluation: ' +str(datetime.datetime.now().replace(microsecond=0)-start_time))
+    return out[0], final_out[0], a.shape[0]
+
+
+def separator_converter(s):
     '''
-    Utility method
+    Utility method: separator corrector for BaseReport.csv files
     '''
     try:
         tmp = float(s.replace(',','.'))
     except:
         tmp = float(s)
     return tmp    
+
+
+def signal_cutter(analog_timestamp_array, signal_array, pre, end):
+    '''
+    analog_timestamp_array is the time array for each trial
+    signal_array could be piezo or heart
+    pre is the OnSet Time Pre Stim -start of recording-
+    end is the Time Go Input -end of the recording-
+    '''
+    start_trial = np.argmin(np.abs(analog_timestamp_array - pre))
+    end_trial = np.argmin(np.abs(analog_timestamp_array - (end + 1000)))#   +1sec for safety
+    print(start_trial)
+    print(end_trial)
+    cut_signal = signal_array[start_trial:end_trial]
+    return cut_signal
+    
