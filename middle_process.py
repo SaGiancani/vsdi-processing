dae8badc5ff6974cfee591d269665975d7df2c41
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2022-07-15 12:42:53 +0000

    -

--- middle_process.py
+++ middle_process.py
@@ -572,7 +572,7 @@ def time_sequence_visualization(start_frame, n_frames_showed, end_frame, data, t
     # Array with indeces of considered frames: it starts from the last considerd zero_frames
     considered_frames = np.round(np.linspace(start_frame-1, end_frame-1, n_frames_showed))
     # Borders for caxis
-    max_bord = np.percentile(data, 77)
+    max_bord = np.percentile(data, 80)
     min_bord = np.percentile(data, 10)
     if log_ is not None:
         print(f'Max value heatmap: {max_bord}')
