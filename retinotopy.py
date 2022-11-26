a274cdbf6590bd932326ce683deace518d12e00f
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2022-11-26 22:55:17 +0000

    -

--- retinotopy.py
+++ retinotopy.py
@@ -258,6 +258,8 @@ class Retinotopy:
                 coords_singl = np.array(list(zip(*centroids_singl)))
                 if coords_singl is not None:
                     # Centroid at maximum response
+                    print(coords_singl)
+                    print(blurred_singl)
                     (a,b), _ = centroid_max(coords_singl[0], coords_singl[1], blurred_singl)
                 # Centroid at the centroid of the polygon given by all the points
                 #(a,b) = centroid_poly(coords_singl[0], coords_singl[1])
