738a962c97ba5af874fb532b8a39ca4ad0cf12d1
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2023-02-20 09:15:47 +0000

    Bug in get_all_blks

--- retinotopy.py
+++ retinotopy.py
@@ -68,7 +68,7 @@ class RetinoSession(md.Session):
             self.cond_names = None
             self.header = super().get_session_header(path_session, spatial_bin, temporal_bin, tolerance, mov_switch, deblank_switch, conditions_id, chunks, strategy, logs_switch)
             # All blks names loaded
-            self.all_blks = super().get_all_blks(self.header['path_session'], sort = True) # all the blks, sorted by creation date -written on the filename-.
+            self.all_blks = md.get_all_blks(self.header['path_session'], sort = True) # all the blks, sorted by creation date -written on the filename-.
             # A blk loaded for useful hyperparameters
             blk = blk_file.BlkFile(os.path.join(self.header['path_session'],'rawdata', self.all_blks[np.random.randint(len(self.all_blks)-1)]), 
                                 self.header['spatial_bin'], 
