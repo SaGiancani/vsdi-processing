be383b0c8aad9664b549c16456097c21258673f3
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2023-09-05 08:15:11 +0000

    bug fixed on indexing

--- process_IOI.py
+++ process_IOI.py
@@ -249,7 +249,7 @@ def operation_among_conditions(maps, sorted_cds_dictionary, start_time, stop_tim
             n_considered_conds = len(coords)/len(y)
 
         # Loop for averaging every n_considered_conds
-        indeces = np.arange(0, len(coords)+1, n_considered_conds, type = int)
+        indeces = np.arange(0, len(coords)+1, n_considered_conds, dtype = int)
         output_matrix_cocktail = np.array([np.nanmean(tmp_data[indeces[i-1]:indeces[i], start_time//frame_time_ext:stop_time//frame_time_ext, :, :], axis = (0, 1)) for i in range (1, len(indeces))])
         cocktail_dict = {(str(a) + '/' +str(i)): b/j for a,b in zip(coords, output_matrix_cocktail) for i, j in zip(coords, output_matrix_cocktail)}
         data_dict = cocktail_dict
