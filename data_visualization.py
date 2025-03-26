eb0b94a26f0860682dfceaeee08cce21be25f52d
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2025-03-26 16:47:49 +0000

    -

--- data_visualization.py
+++ data_visualization.py
@@ -615,10 +615,10 @@ def whole_time_sequence(data,
 
         #ax.legend()
         if title_plot is not None:
-            ax_time.set_title(f'{title_plot}', fontsize = 15)
-        ax_time.tick_params(axis='both', which='major', labelsize=12)
-        ax_time.set_xlabel('Time - ms', fontsize=13)
-        ax_time.set_ylabel('Signal', fontsize=13)
+            ax_time.set_title(f'{title_plot}', fontsize = 20)
+        ax_time.tick_params(axis='both', which='major', labelsize=16)
+        ax_time.set_xlabel('Time - ms', fontsize=18)
+        ax_time.set_ylabel('Signal', fontsize=18)
 
 
     if pixel_spacing is not None:
