c23add2dc761ac9d6ad5586ac751ee09684af133
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2025-04-07 11:01:54 +0000

    -

--- process_vsdi.py
+++ process_vsdi.py
@@ -224,6 +224,7 @@ def get_best_coordinate(image, coords, radius=3):
         # Extract neighborhood
         neighborhood               = image[x_min:x_max, y_min:y_max]
         neighborhood[~np.isfinite(neighborhood)] = np.nanpercentile(image, 10)
+        print(neighborhood)
 
         # Compute average ignoring NaNs
         avg = np.nanmean(neighborhood)
