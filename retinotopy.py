1b5c72724a6c5a614423e78bd6f479ab55d71f3a
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2025-04-08 09:10:06 +0000

    Debugging prints for time sequence size and time windowing in sub

--- retinotopy.py
+++ retinotopy.py
@@ -1027,11 +1027,9 @@ def subtraction_among_conditions(path_session,
                                  fullframe = True, 
                                  single_trial_analysis = True, 
                                  stroke_centroid = None,
-                                 dim_window = 150,
+                                 dim_window = None, # Full frame default
                                  logger = None):                                                         
     
-    if fullframe:
-        dim_window = None
     
     utils.stampa(f'Full frame switch {fullframe}', logger = logger)      
 
@@ -1096,22 +1094,24 @@ def subtraction_among_conditions(path_session,
         
         # Refining the centroid position: after using the control position finding the actual centroid position out of the single trial centroids distribution
         if control_centroid:    # This control also in stroke within AM
-            FOI_reiterated = np.nanmean(pos_inferred_averaged.signal[time_window_inference[0]:time_window_inference[1], :, :], axis=0)#*pos_inferred_averaged.mask
+
+            # If the control centroid has been picked for peaks detection, then recomputing of average centroid
+            utils.stampa(f'The control centroid has been picked: recomputing the centroid from the single trial distribution')
+            utils.stampa(f'Time window inference {time_window_inference} and signal shape {pos_inferred_averaged.signal}', logger=logger)
+            FOI_reiterated = np.nanmean(pos_inferred_averaged.signal[time_window_inference[0]:time_window_inference[1], :, :], axis=0) # Check this signal and the corresponding time window
+            
             # Compute valid bounds
             x_reiterated, y_reiterated = np.ceil(np.nanmean(pos_inferred_averaged.distribution_positions[0])), np.ceil(np.nanmean(pos_inferred_averaged.distribution_positions[1]))
             reduced_window = int(dim_window*.8)
-            utils.stampa(f'Average centroid in distribution: ({x_reiterated, y_reiterated}), Reduced window dim {reduced_window}')
             x_min = int(np.nanmax([0, x_reiterated - reduced_window//2]))
             x_max = int(x_min + 2*(reduced_window//2))
             y_min = int(np.nanmax([0, y_reiterated - reduced_window//2]))
             y_max = int(y_min + 2*(reduced_window//2))
-            utils.stampa(f'Indeces for cropping {(x_min, x_max, y_min, y_max)}')
             FOI_reiterated = FOI_reiterated[y_min:y_max, x_min:x_max]
 
             # Find retinotopic position in averaged signal over 15 frames
             centroids, blobs, _, _ = get_retinotopic_features(FOI_reiterated, mask_switch = False)   
             blb                 = np.zeros((pos_inferred_averaged.signal[0, :, :].shape))         
-            utils.stampa(f'Blobs shape {(blb.shape, blobs.shape)}')
             blb[y_min:y_max, x_min:x_max] = blobs
 
             pos_inferred_averaged.retino_pos     = [centroids[0][0] + x_min, centroids[0][1] + y_min]
