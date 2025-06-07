19166859ab3a67c7fcafd63d6b2c3e626f124eef
SaGiancani <58029664+SaGiancani@users.noreply.github.com>
2025-06-07 14:58:18 +0000

    -

--- st_builder.py
+++ st_builder.py
@@ -822,6 +822,7 @@ class SpatioTemporalSession:
             tmp_baseline, _ = resample_spatiotemporal_map(tmp_baseline, self.time_bin, self.pixel_spacing)
             
             utils.stampa(f'Scale variable: {scale}', logger=self.log)
+            utils.stampa(f'Session: {tmp}, dots {n_dots}, spacing {spacing}, direction {direction}', logger=self.log)
 
             peaks_dict[n_dots][spacing][direction].append(tmp_coord[1]*scale[0])                  
             matrix_dict[n_dots][spacing][direction].append(tmp_map)
